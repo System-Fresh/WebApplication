f1af60682acb9c48258bc06ef5ec4a5e600c9dfb
Systemfresh <systemfresh01@gmail.com>
2024-02-29 17:44:54 +0000

    another-one

--- js/script.js
+++ js/script.js
@@ -19,6 +19,27 @@
         });
 
 
+// This script is for scroll to top function
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  console.log(scrollValue);
+
+  if (pos > 100){
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#800000 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+}
+
+window.onscroll = calcScrollValue;
 
 
 
