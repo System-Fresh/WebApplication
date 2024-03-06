1652cac6a49888cedf17c37641403ad9125660f0
Systemfresh <systemfresh01@gmail.com>
2024-03-06 22:22:54 +0000

    image-filter

--- js/script.js
+++ js/script.js
@@ -53,3 +53,24 @@ window.addEventListener("load", function(){
 });
 
 //   End of GIF Preloader
+
+
+// Select relevant HTML elements
+const filterButtons = document.querySelectorAll("#filter-buttons button");
+const filterableCards = document.querySelectorAll("#filterable-cards .fit-card");
+
+// Function to filter cards based on filter buttons
+const filterCards = (e) => {
+    document.querySelector("#filter-buttons .active").classList.remove("active");
+    e.target.classList.add("active");
+
+    filterableCards.forEach(card => {
+        // show the card if it matches the clicked filter or show all cards if "all" filter is clicked
+        if(card.dataset.name === e.target.dataset.filter || e.target.dataset.filter === "all") {
+            return card.classList.replace("hide", "show");
+        }
+        card.classList.add("hide");
+    });
+}
+
+filterButtons.forEach(button => button.addEventListener("click", filterCards));
